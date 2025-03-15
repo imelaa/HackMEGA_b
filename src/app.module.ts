b6a9dcff60faa38acef7f92a0f6bf725bf758a85
ela <imela2811@gmail.com>
2025-03-14 20:33:19 -0600

    ready for split work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { WasteRecognitionModule } from './waste_recognition/waste_recognition.module';
+import { WasteTrackingModule } from './waste_tracking/waste_tracking.module';
+import { UserRewardModule } from './user_reward/user_reward.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, WasteRecognitionModule, WasteTrackingModule, UserRewardModule, FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
